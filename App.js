bb45e4bac38f3acfac682bd24740e9bd3a558bdf
puneet94 <puneet.mahendra94@gmail.com>
2019-11-24 03:34:45 +1100

    local notifications

--- App.js
+++ App.js
@@ -8,8 +8,7 @@
 
 import React from 'react';
 
-import AsyncStorage from '@react-native-community/async-storage';
-
+var PushNotification = require("react-native-push-notification");
 import {
   StyleSheet,
   
@@ -21,7 +20,7 @@ import {
 import { MenuProvider } from 'react-native-popup-menu';
 import {   createAppContainer } from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack'; 
-
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
 import MainScreen from "./_js/components/main";
 
 import SettingsScreen from "./_js/components/settings";
@@ -56,7 +55,50 @@ const AppNavigator = createAppContainer(createStackNavigator(
 
 
 export default class App extends React.Component {
-
+  componentDidMount(){
+    console.log("hit moubt");
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function(token) {
+        console.log("TOKEN:", token);
+      },
+    
+      // (required) Called when a remote or local notification is opened or received
+      onNotification: function(notification) {
+        console.log("NOTIFICATION:", notification);
+    
+        // process the notification
+    
+        // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+    
+      // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
+      senderID: "YOUR GCM (OR FCM) SENDER ID",
+    
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+      },
+    
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+    
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       */
+      requestPermissions: true
+    });
+    PushNotification.checkPermissions(function(data){
+      console.log("persmissoon fata");
+      console.log(data);
+    }) 
+  }
   render() {
     return (
       <MenuProvider>
